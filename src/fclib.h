8d03864ddc262ecfafb498f9232ed6e092f0df06
acary <acary@99398ccb-1c7c-447f-b8ad-29092ce852eb>
2011-11-23 10:31:26 +0000

    add GPL licence and TODO list (org file)

--- src/fclib.h
+++ src/fclib.h
@@ -1,5 +1,22 @@
+/* FCLIB Copyright (C) 2011 FClib project
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: fclib-project@lists.gforge.inria.fr   
+ */
 /* 
- * fclib.h
+ * Fclib.h
  * -----------------------------------------
  * frictional contact library interface
  */
