d2c1c4b189be8561b10dc6f1db8603f6861f8602
bremond <bremond@99398ccb-1c7c-447f-b8ad-29092ce852eb>
2014-02-06 11:44:05 +0000

    fix mathjax in doxygen

--- src/fclib.h
+++ src/fclib.h
@@ -1,5 +1,5 @@
 /* FCLIB Copyright (C) 2011 FClib project
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -13,30 +13,30 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Contact: fclib-project@lists.gforge.inria.fr   
+ * Contact: fclib-project@lists.gforge.inria.fr
  */
 
-/*!\file fclib.h 
+/*!\file fclib.h
  * -----------------------------------------
  * frictional contact library interface
  * -----------------------------------------
- * 
+ *
  * This C API provides functions to read and write Frictional contact problemes in HDF5 format
  * Two kind of problem are considered
  *  Given
  * <ul>
- *   <li> a symmetric positive semi--definite  matrix \f${W} \in \nbR^{m \times m} \f$ </li>
- *   <li> a vector \f$ {q} \in \nbR^m\f$</li>
- *   <li> a vector of coefficients of friction \f$\mu \in\nbR^{n_c}\f$</li>
+ *   <li> a symmetric positive semi--definite  matrix \f${W} \in {\mathrm{I\!R}}^{m \times m} \f$ </li>
+ *   <li> a vector \f$ {q} \in {\mathrm{I\!R}}^m\f$</li>
+ *   <li> a vector of coefficients of friction \f$\mu \in{\mathrm{I\!R}}^{n_c}\f$</li>
  *</ul>
- * the local FC problem  is to find two vectors \f$u\in{\ensuremath{\mathrm{I\!R}}}^m\f$, 
- * the relative local velocity and \f$r\in {\ensuremath{\mathrm{I\!R}}}^m\f$, 
+ * the local FC problem  is to find two vectors \f$u\in{\ensuremath{\mathrm{I\!R}}}^m\f$,
+ * the relative local velocity and \f$r\in {\ensuremath{\mathrm{I\!R}}}^m\f$,
  * the contact forces denoted by \f$\mathrm{FC}(W,q,\mu)\f$  such that
  * \f{eqnarray*}{
  * \begin{cases}
  *    \hat u = W r + q +\left[
  *      \left[\begin{array}{c}
- *          \mu^\alpha \|u^\alpha_\t\|\         \
+ *          \mu^\alpha \|u^\alpha_T\|\         \
  *         0 \                                  \
  *         0
  *        \end{array}\right]^T, \alpha = 1 \ldots n_c
@@ -44,38 +44,38 @@
  *    C^\star_{\mu} \ni {\hat u} \perp r \in C_{\mu}
  * \end{cases}
  * \f}
- * where the Coulomb friction cone for a  contact \f$\alpha\f$ is defined by 
+ * where the Coulomb friction cone for a  contact \f$\alpha\f$ is defined by
  * \f{eqnarray*}{
  * \label{eq:CCC}
- * C_{\mu^\alpha}^{\alpha}  = \{r^\alpha, \|r^\alpha_\t \| \leq \mu^\alpha |r^\alpha_\n| \}
+ * C_{\mu^\alpha}^{\alpha}  = \{r^\alpha, \|r^\alpha_T \| \leq \mu^\alpha |r^\alpha_N| \}
  *\f}
  * and the set \f$C^{\alpha,\star}_{\mu^\alpha}\f$ is its dual.
  * We are also dealing with global  FC problem defined by
- * Given 
+ * Given
  * <ul>
- *    <li> a symmetric positive definite matrix \f${M} \in \nbR^{n \times n}\f$</li>
- *   <li> a vector \f$ {f} \in \nbR^n\f$,</li>
- *   <li> a matrix  \f${H} \in \nbR^{n \times m}\f$</li>
- *   <li> a matrix  \f${G} \in \nbR^{n \times p}\f$</li>
- *    <li> a vector \f$w \in \nbR^{m}\f$,</li>
- *    <li> a vector \f$b \in \nbR^{p}\f$,</li>
- *   <li> a vector of coefficients of friction \f$\mu \in \nbR^{n_c}\f$
+ *    <li> a symmetric positive definite matrix \f${M} \in {\mathrm{I\!R}}^{n \times n}\f$</li>
+ *   <li> a vector \f$ {f} \in {\mathrm{I\!R}}^n\f$,</li>
+ *   <li> a matrix  \f${H} \in {\mathrm{I\!R}}^{n \times m}\f$</li>
+ *   <li> a matrix  \f${G} \in {\mathrm{I\!R}}^{n \times p}\f$</li>
+ *    <li> a vector \f$w \in {\mathrm{I\!R}}^{m}\f$,</li>
+ *    <li> a vector \f$b \in {\mathrm{I\!R}}^{p}\f$,</li>
+ *   <li> a vector of coefficients of friction \f$\mu \in {\mathrm{I\!R}}^{n_c}\f$
  *</ul>
- * the Global Mixed 3DFC problem  is to find four vectors \f$ {v} \in \nbR^n\f$, \f$u\in\nbR^m\f$, \f$r\in \nbR^m\f$ and \f$\lambda \in \nbR^p\f$ denoted by \f$\mathrm{GM3DFC}(M,H,G,w,b,\mu)\f$  such that
+ * the Global Mixed 3DFC problem  is to find four vectors \f$ {v} \in {\mathrm{I\!R}}^n\f$, \f$u\in{\mathrm{I\!R}}^m\f$, \f$r\in {\mathrm{I\!R}}^m\f$ and \f$\lambda \in {\mathrm{I\!R}}^p\f$ denoted by \f$\mathrm{GM3DFC}(M,H,G,w,b,\mu)\f$  such that
  * \f{eqnarray*}{
  * \begin{cases}
  *   M v = {H} {r} + G\lambda + {f} \\ \        \
  *   G^T v +b =0 \\ \                           \
  *   \hat u = H^T v + w +\left[
  *     \left[\begin{array}{c}
- *       \mu \|u^\alpha_\t\|\                   \
+ *       \mu \|u^\alpha_T\|\                   \
  *       0 \                                    \
  *       0
  *     \end{array}\right]^T, \alpha = 1 \ldots n_c
  * \right]^T \\ \                                    \
  *   C^\star_{\mu} \ni {\hat u} \perp r \in C_{\mu}
  * \end{cases}
- * \f} 
+ * \f}
  *
  */
 
@@ -85,7 +85,7 @@
 #include "csparse.h"
 
 /**\struct  fclib_info fclib.h
- * This structure allows the user to enter a  problem information  as a title, a short description and known mathematical properties of the problem 
+ * This structure allows the user to enter a  problem information  as a title, a short description and known mathematical properties of the problem
  */
 
 struct fclib_info
@@ -119,19 +119,19 @@ struct fclib_matrix_info  /* matrix information */
 struct fclib_matrix   /*  */
 {
   /** maximum number of entries */
-  int nzmax ;	   
+  int nzmax ;
   /** number of rows */
-  int m ;	    
+  int m ;
   /** number of columns */
-  int n ;	   
+  int n ;
   /** compressed: row (size m+1) or column (size n+1) pointers; triplet: row indices (size nz) */
-  int *p ;	 
-  /** compressed: column or row indices, size nzmax; triplet: column indices (size nz) */  
-  int *i ;	 
+  int *p ;
+  /** compressed: column or row indices, size nzmax; triplet: column indices (size nz) */
+  int *i ;
   /** numerical values, size nzmax */
-  double *x ;	   
+  double *x ;
   /** # of entries in triplet matrix,   -1 for compressed columns,  -2 for compressed rows    */
-  int nz ;	
+  int nz ;
   /** info for this matrix */
   struct fclib_matrix_info *info;
 };
@@ -141,33 +141,33 @@ struct fclib_matrix   /*  */
  *
  * Given
  * <ul>
- *    <li> a symmetric positive definite matrix \f${M} \in \nbR^{n \times n}\f$</li>
- *   <li> a vector \f$ {f} \in \nbR^n\f$,</li>
- *   <li> a matrix  \f${H} \in \nbR^{n \times m}\f$</li>
- *   <li> a matrix  \f${G} \in \nbR^{n \times p}\f$</li>
- *    <li> a vector \f$w \in \nbR^{m}\f$,</li>
- *    <li> a vector \f$b \in \nbR^{p}\f$,</li>
- *   <li> a vector of coefficients of friction \f$\mu \in \nbR^{n_c}\f$
+ *    <li> a symmetric positive definite matrix \f${M} \in {\mathrm{I\!R}}^{n \times n}\f$</li>
+ *   <li> a vector \f$ {f} \in {\mathrm{I\!R}}^n\f$,</li>
+ *   <li> a matrix  \f${H} \in {\mathrm{I\!R}}^{n \times m}\f$</li>
+ *   <li> a matrix  \f${G} \in {\mathrm{I\!R}}^{n \times p}\f$</li>
+ *    <li> a vector \f$w \in {\mathrm{I\!R}}^{m}\f$,</li>
+ *    <li> a vector \f$b \in {\mathrm{I\!R}}^{p}\f$,</li>
+ *   <li> a vector of coefficients of friction \f$\mu \in {\mathrm{I\!R}}^{n_c}\f$
  *</ul>
- * the Global Mixed 3DFC problem  is to find four vectors \f$ {v} \in \nbR^n\f$, \f$u\in\nbR^m\f$, \f$r\in \nbR^m\f$ and \f$\lambda \in \nbR^p\f$ denoted by \f$\mathrm{GM3DFC}(M,H,G,w,b,\mu)\f$  such that
+ * the Global Mixed 3DFC problem  is to find four vectors \f$ {v} \in {\mathrm{I\!R}}^n\f$, \f$u\in{\mathrm{I\!R}}^m\f$, \f$r\in {\mathrm{I\!R}}^m\f$ and \f$\lambda \in {\mathrm{I\!R}}^p\f$ denoted by \f$\mathrm{GM3DFC}(M,H,G,w,b,\mu)\f$  such that
  * \f{eqnarray*}{
  * \begin{cases}
  *   M v = {H} {r} + G\lambda + {f} \\ \        \
  *   G^T v +b =0 \\ \                           \
  *   \hat u = H^T v + w +\left[
  *     \left[\begin{array}{c}
- *       \mu \|u^\alpha_\t\|\                   \
+ *       \mu \|u^\alpha_T\|\                   \
  *       0 \                                    \
  *       0
  *     \end{array}\right]^T, \alpha = 1 \ldots n_c
  * \right]^T \\ \                                    \
  *   C^\star_{\mu} \ni {\hat u} \perp r \in C_{\mu}
  * \end{cases}
- * \f} 
- * where the Coulomb friction cone for a  contact \f$\alpha\f$ is defined by 
+ * \f}
+ * where the Coulomb friction cone for a  contact \f$\alpha\f$ is defined by
  * \f{eqnarray*}{
  * \label{eq:CCC}
- * C_{\mu^\alpha}^{\alpha}  = \{r^\alpha, \|r^\alpha_\t \| \leq \mu^\alpha |r^\alpha_\n| \}
+ * C_{\mu^\alpha}^{\alpha}  = \{r^\alpha, \|r^\alpha_T \| \leq \mu^\alpha |r^\alpha_N| \}
  *\f}
  * and the set \f$C^{\alpha,\star}_{\mu^\alpha}\f$ is its dual.
  */
@@ -181,14 +181,14 @@ struct fclib_global
   struct fclib_matrix *G;
   /** the vector \f$\mu\f$ of coefficient of friction (see mathematical description below)*/
   double *mu;
-  /** the vector f (see mathematical description below)*/  
+  /** the vector f (see mathematical description below)*/
   double *f;
   /** the vector b (see mathematical description below)*/
   double *b;
   /** the vector w (see mathematical description below)*/
   double *w;
   /** the dimension , 2 or 3, of the local space at contact (2d or 3d friction contact laws)*/
-  int spacedim;             
+  int spacedim;
   /** info on the problem */
   struct fclib_info *info;
 };
@@ -197,20 +197,20 @@ struct fclib_global
  *
  * given
  * <ul>
- *   <li> a positive semi--definite matrix  \f${W} \in \nbR^{m \times m}\f$</li>
- *   <li> a matrix  \f${V} \in \nbR^{m \times p}\f$</li>
- *   <li> a matrix  \f${R} \in \nbR^{p \times p}\f$</li>
- *   <li> a vector \f$q \in \nbR^{m}\f$,</li>
- *   <li> a vector \f$s \in \nbR^{p}\f$,</li>
- *   <li> a vector of coefficients of friction \f$\mu \in \nbR^{n_c}\f$
- * </ul
- * the  Mixed 3DFC problem  is to find three vectors \f$u\in\nbR^m\f$, \f$r\in \nbR^m\f$ and \f$\lambda \in \nbR^p\f$ denoted by \f$\mathrm{M3DFC}(R,V,W,q,s,\mu)\f$  such that
+ *   <li> a positive semi--definite matrix  \f${W} \in {\mathrm{I\!R}}^{m \times m}\f$</li>
+ *   <li> a matrix  \f${V} \in {\mathrm{I\!R}}^{m \times p}\f$</li>
+ *   <li> a matrix  \f${R} \in {\mathrm{I\!R}}^{p \times p}\f$</li>
+ *   <li> a vector \f$q \in {\mathrm{I\!R}}^{m}\f$,</li>
+ *   <li> a vector \f$s \in {\mathrm{I\!R}}^{p}\f$,</li>
+ *   <li> a vector of coefficients of friction \f$\mu \in {\mathrm{I\!R}}^{n_c}\f$
+ * </ul>
+ * the  Mixed 3DFC problem  is to find three vectors \f$u\in{\mathrm{I\!R}}^m\f$, \f$r\in {\mathrm{I\!R}}^m\f$ and \f$\lambda \in {\mathrm{I\!R}}^p\f$ denoted by \f$\mathrm{M3DFC}(R,V,W,q,s,\mu)\f$  such that
  * \f{eqnarray*}\label{eq:lcp1}
  *\begin{cases}
  *   V^T {r} + R \lambda + s = 0 \\ \           \
  *   \hat u = W {r}    + V\lambda  + q +\left[
  *     \left[\begin{array}{c}
- *       \mu^\alpha \|u^\alpha_\t\|\            \
+ *       \mu^\alpha \|u^\alpha_T\|\            \
  *       0 \                                    \
  *       0
  *     \end{array}\right]^T, \alpha = 1 \ldots n_c
@@ -218,11 +218,11 @@ struct fclib_global
  *   C^\star_{\mu} \ni {\hat u} \perp r \in C_{\mu}
  * \end{cases}
  * \f}
- * where the Coulomb friction cone for a  contact \f$\alpha\f$ is defined by 
+ * where the Coulomb friction cone for a  contact \f$\alpha\f$ is defined by
  * \f{eqnarray*}{
  * \label{eq:CCC}
- * C_{\mu^\alpha}^{\alpha}  = \{r^\alpha, \|r^\alpha_\t \| \leq \mu^\alpha |r^\alpha_\n| \}
- *\f}
+ * C_{\mu^\alpha}^{\alpha}  = \{r^\alpha, \|r^\alpha_T \| \leq \mu^\alpha |r^\alpha_N| \}
+ * \f}
  * and the set \f$C^{\alpha,\star}_{\mu^\alpha}\f$ is its dual.
  */
 struct fclib_local
@@ -238,7 +238,7 @@ struct fclib_local
   /** the vector q (see mathematical description below)*/
   double *q;
   /** the vector s (see mathematical description below)*/
-  double *s; 
+  double *s;
   /** the dimension , 2 or 3, of the local space at contact (2d or 3d friction contact laws)*/
   int spacedim;             /* 2 or 3 */
   /** info on the problem */
@@ -248,23 +248,23 @@ struct fclib_local
 /** \struct fclib_solution fclib.h
  *  A solution or a guess for the frictional contact problem.
  *
- * This structure allows to store a solution vector of a guess vector for the 
+ * This structure allows to store a solution vector of a guess vector for the
  * various frictional contact problems.
  */
 struct fclib_solution /* solution data */
 {
   /** global velocity (or position/displacement for quasi-static problems) solution vector */
-  double *v;   
-  /** local velocity  (or position/displacement for quasi-static problems) solution vector */  
+  double *v;
+  /** local velocity  (or position/displacement for quasi-static problems) solution vector */
   double *u;
-  /** local contact forces  (or impulses) solution vector */  
+  /** local contact forces  (or impulses) solution vector */
   double *r;
-  /** multiplier for equlity constraints (\f$\lambda\f$) solution vector */  
+  /** multiplier for equlity constraints (\f$\lambda\f$) solution vector */
   double *l;
 };
 
 /** \enum  fclib_merit
- * MERIT_1 is a implementation of the merit function based on the natural map for a SOCCP   
+ * MERIT_1 is a implementation of the merit function based on the natural map for a SOCCP
  */
 enum fclib_merit {MERIT_1, MERIT_2} ; /* merit functions */
 
@@ -310,7 +310,7 @@ double fclib_merit_local (struct fclib_local *problem, enum fclib_merit merit, s
 /** delete global problem */
 void fclib_delete_global (struct fclib_global *problem);
 
-/** \fn delete local problem */
+/** delete local problem */
 void fclib_delete_local (struct fclib_local *problem);
 
 /** delete solutions or guesses */
