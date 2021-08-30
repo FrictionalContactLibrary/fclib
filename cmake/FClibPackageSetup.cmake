3b493e2c71ac1dff95bc0a699b37096be9301e36
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2021-08-30 16:44:18 +0200

    Update cmake setup in fclib - fix stuff related to hdf5 parallel - add conf to allow usage of FetchContent in siconos (--> automatic install of fclib during setup)

--- cmake/FClibPackageSetup.cmake
+++ cmake/FClibPackageSetup.cmake
@@ -3,9 +3,9 @@ configure_file(
   "${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
   "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake"
   IMMEDIATE @ONLY)
-add_custom_target(uninstall
-  echo >> ${CMAKE_CURRENT_BINARY_DIR}/install_manifest.txt
-  COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake)
+#add_custom_target(uninstall
+#  echo >> ${CMAKE_CURRENT_BINARY_DIR}/install_manifest.txt
+#  COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake)
 
 # ===== Package configuration file ====
 # https://cmake.org/cmake/help/latest/manual/cmake-packages.7.html#creating-packages
@@ -13,12 +13,12 @@ add_custom_target(uninstall
 include(CMakePackageConfigHelpers)
 
 # Generate ${PROJECT_NAME}Config.cmake
-configure_package_config_file(fclib-config.cmake.in ${CMAKE_BINARY_DIR}/fclib-config.cmake
+configure_package_config_file(fclib-config.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/fclib-config.cmake
   INSTALL_DESTINATION ${ConfigPackageLocation})
 
 # Generate fclib-config-version.cmake file.
 write_basic_package_version_file(
-  "${CMAKE_BINARY_DIR}/fclib-config-version.cmake"
+  "${CMAKE_CURRENT_BINARY_DIR}/fclib-config-version.cmake"
   VERSION ${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}
   COMPATIBILITY SameMajorVersion
   )
@@ -36,7 +36,7 @@ endif()
 
 # install config files
 install(
-  FILES ${CMAKE_BINARY_DIR}/fclib-config.cmake ${CMAKE_BINARY_DIR}/fclib-config-version.cmake
+  FILES ${CMAKE_CURRENT_BINARY_DIR}/fclib-config.cmake ${CMAKE_CURRENT_BINARY_DIR}/fclib-config-version.cmake
   DESTINATION ${ConfigPackageLocation})
 
 
@@ -47,12 +47,12 @@ if(NOT SKIP_PKGCONFIG)
     set(PKGCONFIG_LIBS "-L\${libdir} -lfclib")
   endif()
   configure_file(
-    "${CMAKE_SOURCE_DIR}/${PROJECT_NAME}.pc.in"
-    "${CMAKE_BINARY_DIR}/${PROJECT_NAME}.pc"
+    "${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}.pc.in"
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.pc"
     @ONLY)
   set(INSTALL_PKGCONFIG_DIR "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/pkgconfig"
     CACHE PATH "Installation directory for pkgconfig (.pc) files")
-  install(FILES ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.pc
+  install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.pc
     DESTINATION "${INSTALL_PKGCONFIG_DIR}")
 endif()
 
